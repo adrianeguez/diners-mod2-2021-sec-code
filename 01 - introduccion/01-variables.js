40905c25f2fc3e5291ad1daa5b06d291a38cd5f5
Adrian <eadepto@hotmail.com>
2021-09-21 20:40:47 -0500

    hasta arreglos

--- 01 - introduccion/01-variables.js
+++ 01 - introduccion/01-variables.js
@@ -35,6 +35,81 @@ console.log(typeof apellido);
 console.log(typeof Number("asd")); // number
 console.log(Number("fadsa")); // NaN
 
+// Truty Falsy
+
+if(true == true){
+    console.log("Es verdadero"); //!
+}else{
+    console.log("Es Falso");
+}
+if(""){
+    console.log("Es verdadero");
+}else{
+    console.log("Es Falsy"); //!
+}
+if("Adrian"){
+    console.log("Es truty"); //!
+}else{
+    console.log("Es Falso");
+}
+
+if(-1){
+    console.log("Es truty"); // Truty
+}else{
+    console.log("Es Falso");
+}
+if(0){
+    console.log("Es truty");
+}else{
+    console.log("Es Falso"); // Falsy
+}
+if(1){
+    console.log("Es truty"); // Truty
+}else{
+    console.log("Es Falso");
+}
+
+// JSON
+
+const adrian = {
+    nombre: "Adrian", // llave: valor,
+    apellido: 'Eguez',
+    edad: 32,
+    hijos: null,
+    zapatos: undefined,
+    casado: false,
+    ropa: {
+        color: 'plomo',
+        talla: '40',
+    },
+    mascotas: ['Cachetes', 'Pequitas', 'Panda'],
+}; // object
+
+adrian.nombre; // "Adrian"
+adrian["nombre"]; // "Adrian"
+adrian.nombre = "Vicente";
+if(adrian){ // Codificacion Defensiva
+    if(adrian.ropa){
+        console.log(adrian.ropa.color);
+        console.log(adrian["ropa"]["color"]);
+    }
+}
+console.log(adrian);
+console.log(Object.keys(adrian)); // llaves
+console.log(Object.values(adrian)); // valores
+
+// Arreglos
+
+const arreglo = [6, 7, 8, 9, 10];
+
+const arregloTodo = [
+    true, 1, 1.2, "Adrian", undefined, null, {}, [1, 2, true, "A"]
+];
+
+
+
+
+
 
 
 
