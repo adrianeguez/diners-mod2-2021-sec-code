e113af9084249edfbf126f39cc7ad5f8701a22dd
Adrian <eadepto@hotmail.com>
2021-09-21 19:40:55 -0500

    hasta variables

---
+++ 01 - introduccion/01-variables-typescript.ts
@@ -0,0 +1,16 @@
+// 01-variables.js
+console.log('Hola mundo');
+
+// Declarar variables
+
+// Mutables -> = -> reasignadas
+
+var variableUno = 1;
+let variableDos = 2;
+variableUno = 3;
+variableDos = 3;
+
+// Inmutables -> no = -> no reasignadas
+
+const noPuedoCambiar = 1;
+noPuedoCambiar = "212";
---
+++ 01 - introduccion/01-variables.js
@@ -0,0 +1,50 @@
+// 01-variables.js
+console.log('Hola mundo');
+
+// Declarar variables
+
+// Mutables -> = -> reasignadas
+
+var variableUno = 1;
+let variableDos = 2;
+variableUno = 3;
+variableDos = 3;
+
+// Inmutables -> no = -> no reasignadas
+
+const noPuedoCambiar = 1;
+// noPuedoCambiar = "212";
+
+// Tipos de variables
+
+const numero = 1; // number
+const sueldo = 1.2; // number
+const texto = "Adrian"; // string
+const apellido = 'Eguez'; // string
+const booleanooo = false; // boolean
+const hijos = null; // object
+const zapatos = undefined; // undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleanooo);
+console.log(typeof hijos);
+console.log(typeof zapatos);
+console.log(typeof apellido);
+console.log(typeof Number("asd")); // number
+console.log(Number("fadsa")); // NaN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
