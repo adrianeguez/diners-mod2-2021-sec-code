0d22c076fdd01417f0326b4a7d3ef2b736cf2484
Adrian <eadepto@hotmail.com>
2021-09-23 19:38:44 -0500

    ejercicio callbacks

--- 01 - introduccion/02-callback.js
+++ 01 - introduccion/02-callback.js
@@ -1,14 +1,33 @@
-const fs = require('fs');
+const fs = require('fs'); // importar paquete fs
 console.log('Primero');
+
+// leer archivo './01-variables.js' CONTENIDO_UNO
+// leer archivo './01-variables-typescript.ts' (cualquir otro archivo) CONTENIDO_DOS
+// escribir nuevo archivo 04-ejercicio.txt CONTENIDO_UNO + CONTENIDO_DOS
+
 
 fs.readFile( // Asincronas
     './01-variables.js', // path
     'utf-8', // codificacion
-    (error, contenido) => {
-        console.log('Error: ', error);
+    (error, contenido) => { // CALLBACK
+        console.log('Error rf: ', error);
         // console.log('Contenido: ', contenido);
         console.log('Segundo');
     }
-)
+);
+
+fs.writeFile( // Asincronas
+    './03-ejemplo.txt', // nombre archivo
+    'Hola mundo', // Contenido del archivo
+    'utf-8', // Codificacion
+    (error)=>{ // CALLBACK
+        if(error){
+            console.error('Error wf: ', error);
+        }else{
+            console.log('archivo creado');
+        }
+    }
+);
+
 
 console.log('Tercero')
